df8fe0447ab82ac083c86708cdd3098d1671a791
Braden <bmw02002turbo@gmail.com>
2021-01-25 16:12:19 -0800

    Added text field with username

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <v-app>
     <v-card>
       <v-card-title> <h1 class="display-1">Hello</h1></v-card-title>
+      <v-card-text
+        ><v-form><v-text-field label="username"></v-text-field></v-form
+      ></v-card-text>
     </v-card>
   </v-app>
 </template>
