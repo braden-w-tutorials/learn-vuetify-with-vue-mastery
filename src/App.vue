ec5801272fc53ca8a9a225f1ba9a86e8ede02df9
Braden <bmw02002turbo@gmail.com>
2021-01-25 16:15:20 -0800

    Added password

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <v-app>
     <v-card>
       <v-card-title> <h1 class="display-1">Hello</h1></v-card-title>
-      <v-card-text
-        ><v-form><v-text-field label="username"></v-text-field></v-form
-      ></v-card-text>
+      <v-card-text>
+        <v-form
+          ><v-text-field label="Username"></v-text-field>
+          <v-text-field label="Password" type="password"></v-text-field>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-app>
 </template>
