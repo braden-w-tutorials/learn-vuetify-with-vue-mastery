f5d17e0ad9293671ff6aac1b3c6db1717aa4c34b
Braden <bmw02002turbo@gmail.com>
2021-01-25 16:21:29 -0800

    Added class mt-5 to move modal a little down

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto">
+    <v-card width="400" class="mx-auto mt-5">
       <v-card-title> <h1 class="display-1">Hello</h1></v-card-title>
       <v-card-text>
         <v-form
